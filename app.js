ceaaa12e6b0ce53c04a6ed0a0fc5a73c2db878a3
aatifkatote <aatifkatote@gmail.com>
2021-09-02 20:14:18 +0530

    data types

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // Data types
 // Primitive Data Types
 
-let age = 289.213412349012384908
-let Name = 'aasdfasdf234234@|'
-let isTrue = false
-let isfalse = false
+let age = 19;
+console.log(age)
+console.log(typeof age)
 
-// Non primitive data types
+let brand
+console.log(brand)
 
-let arr = [1, 2, 3, 4, 5]
-arr.push("value")
+let Name ="Aatif";
+console.log(Name)
+console.log(typeof Name)
+
+
+//non-primitive
+
+let arr = [1,2,3,4,5]
+arr.push("hello")
+arr.unshift(true)
 console.log(arr)
 console.log(typeof arr)
