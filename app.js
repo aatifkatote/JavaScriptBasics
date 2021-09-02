991fb894fe8830d15f4884db9778ed5cbe6c6a42
aatifkatote <aatifkatote@gmail.com>
2021-09-02 19:26:06 +0530

    Updated Java Setup

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('Hey this is my first piece of JS!!! YAY')
+
+console.log(12345678901234)
+
+console.log({ name: "Yutika" })
+
+console.log(true)
+console.log(false)
+
+console.log(false, 234243, 'string', { name: "Alfred" })
+
+console.warn("This is a warning")
+
+console.error("This is an error")
