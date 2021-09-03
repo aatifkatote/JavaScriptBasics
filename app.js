264c1db719d1d10bdcef51e8d0f37824495ed533
aatifkatote <aatifkatote@gmail.com>
2021-09-03 20:06:36 +0530

    Comparison Operators

--- app.js
+++ app.js
@@ -29,3 +29,24 @@ let x = 19
  console.log(x)
  console.log(typeof x)
 
+ // Comparison operators
+
+const a = 21
+const b = 7
+const c = 21
+
+console.log('a > b', a > b)
+console.log('a < b', a < b)
+console.log('a == b', a == b)
+console.log('a == c', a == c)
+console.log('a>=c', a >= c)
+
+console.log('--------------------------------')
+// --------------------------------
+
+const e = 23
+const d = '23'
+
+console.log(e == d)
+console.log(e === d)
+
