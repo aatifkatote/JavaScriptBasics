7d136f53a0d2d9454766cdc870e68afbfa2597f0
aatifkatote <aatifkatote@gmail.com>
2021-09-03 20:29:57 +0530

    Logical Operators

--- app.js
+++ app.js
@@ -17,10 +17,14 @@ console.log(b % a)
 
 console.log('-------------------------------------------')
 
-//Concatenation 
-const c= "3"
-const d= 3
-console.log(c-d)
+let c = 5
+
+c *= 5
+
+// c operator=number is equivalent to c = c operator number
+// c = c * 5 = 5*5 = 25
+
+console.log(c)
 
 //Assignment Operators
 let n= "Aatif"
@@ -30,16 +34,15 @@ let x = 19
  console.log(typeof x)
 
  // Comparison operators
+const i = 21
+const j = 7
+const k = 21
 
-const a = 21
-const b = 7
-const c = 21
-
-console.log('a > b', a > b)
-console.log('a < b', a < b)
-console.log('a == b', a == b)
-console.log('a == c', a == c)
-console.log('a>=c', a >= c)
+console.log('i > j', i > j)
+console.log('a < j', i < j)
+console.log('i == j', i == j)
+console.log('i == k', i == k)
+console.log('i>=c', i >= k)
 
 console.log('--------------------------------')
 // --------------------------------
@@ -49,4 +52,34 @@ const d = '23'
 
 console.log(e == d)
 console.log(e === d)
+
+// Logical operators
+
+const w = 10
+const y = 50
+
+const z = 55
+
+console.log(z > w && z < y)
+
+// console.log('a == b', a == b)
+// console.log('a != b', a != b)
+// console.log('')
+
+// console.log('Truth table for AND')
+// console.log('')
+
+// console.log('false && false', false && false)
+// console.log('true && true', true && true)
+// console.log('true && false', true && false)
+// console.log('false && true', false && true)
+// console.log('')
+
+// console.log('Truth table for OR')
+// console.log('')
+
+// console.log('false|| false', false || false)
+// console.log('true|| true', true || true)
+// console.log('true|| false', true || false)
+// console.log('false|| true', false || true)
 
