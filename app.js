98baf96796531c708b4f1fdb86a1084f283c3cf3
aatifkatote <aatifkatote@gmail.com>
2021-09-07 21:41:23 +0530

    DOM selectors

--- app.js
+++ app.js
@@ -1,84 +1,15 @@
-//   Arithmetic operators
+// const element = document.getElementById('selectme')
 
-const a = 2
-const b = 15
+// const element = document.getElementsByClassName('heading')
 
-console.log(a + b)
+// const element = document.getElementsByTagName('h2')
 
-console.log(a - b)
+//const element = document.querySelector('.heading')
+//QuerySelector only returns the first match
 
-console.log(a * b)
+//const element = document.querySelectorAll('.heading')
+//QuerySelectorAll returns all of the matches similar to array
 
-console.log(a / b)
+const element = document.querySelector('h3.heading')
 
-console.log(a ** b)
-
-console.log(b % a)
-
-console.log('-------------------------------------------')
-
-let c = 5
-
-c *= 5
-
-// c operator=number is equivalent to c = c operator number
-// c = c * 5 = 5*5 = 25
-
-console.log(c)
-
-//Assignment Operators
-let n= "Aatif"
-let x = 19
- x+=n
- console.log(x)
- console.log(typeof x)
-
- // Comparison operators
-const i = 21
-const j = 7
-const k = 21
-
-console.log('i > j', i > j)
-console.log('i < j', i < j)
-console.log('i == j', i == j)
-console.log('i == k', i == k)
-console.log('i>=k', i >= k)
-
-console.log('--------------------------------')
-// --------------------------------
-
-const e = 23
-const d = '23'
-
-console.log(e == d)
-console.log(e === d)
-
-// Logical operators
-
-const w = 10
-const y = 50
-
-const z = 55
-
-console.log(z > w && z < y)
-
-// console.log('a == b', a == b)
-// console.log('a != b', a != b)
-// console.log('')
-
-// console.log('Truth table for AND')
-// console.log('')
-
-// console.log('false && false', false && false)
-// console.log('true && true', true && true)
-// console.log('true && false', true && false)
-// console.log('false && true', false && true)
-// console.log('')
-
-// console.log('Truth table for OR')
-// console.log('')
-
-// console.log('false|| false', false || false)
-// console.log('true|| true', true || true)
-// console.log('true|| false', true || false)
-// console.log('false|| true', false || true)
+console.log(element)
