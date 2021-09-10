350e206e87aa6992e0282d9c4f265e01f5a2dbd5
aatifkatote <aatifkatote@gmail.com>
2021-09-10 19:27:04 +0530

    for and while loops

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
-const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissor = document.querySelector('#scissor')
-const board = document.querySelector('#board')
+//for loop
 
-const handler = (event) => {
-    const action = event.target.id
-    // if(action === 'rock'){
-    //     board.innerHTML = "Rock crushes Scissor"
-    // }
-    // else if(action === 'paper'){
-    //     board.innerHTML = "Paper traps Rock"
-    // }
-    // else if(action === 'scissor'){
-    //     board.innerHTML = "Scissor cuts Paper"
-    // }
-    // else{
-    //     board.innerHTML = "Click Something"
-    // }
-    switch (action) {
-        case "rock":
-            board.innerHTML = "Rock crushes scissors"
-            break
-        case 'paper':
-            board.innerHTML = "Paper traps Rock"
-            break
-        case 'scissor':
-            board.innerHTML = "Scissor cuts paper"
-            break
-        default:
-            board.innerHTML = "Click something"
-    }
-
+for (var i=0; i<10; i++)
+{   var age =19
+    console.log(i)
 }
 
-rock.addEventListener('click', handler)
-paper.addEventListener('click', handler)
-scissor.addEventListener('click', handler)
+// while loop
+
+let iteration = 0
+
+while (iteration < 10){
+    console.log(iteration)
+    iteration++
+
+}
