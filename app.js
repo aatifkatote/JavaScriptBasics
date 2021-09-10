22dc29f4ed6094c248e2c8937e4d53c30fccf01b
aatifkatote <aatifkatote@gmail.com>
2021-09-10 20:20:12 +0530

    Array Iterations

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-// Array & Array methods
-
-const balls = document.querySelectorAll('balls')
-// const balls = document.getElementsByClassName('balls')
-const body = document.querySelector('body')
-
+//Iteration over array
 const arr = [1, 2 , 3, 4, 5]
-console.log(arr[0])
 
-while (arr.length < 15){
-    arr.push("iteration")
-    console.log(arr.length)
-}
-console.log(arr)
-
-const added = arr.includes(100)
-console.log(added)
-
-// const added = arr.unshift(100) (adds element at the beginning of array while push adds at end of array)
-// console.log(arr)
-
-// arr.shift()  (removes first element)
-// arr.shift()  (can be used multiple times to keep removing first element)
+// Basic Method for printing
+// for(var i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
 
 
-// arr.splice(2)  (used to remove element from specific index)
-// console.log(arr)
+
+// For Each
+arr.forEach((element, iteration) => {
+    console.log('iteration')
+    console.log(element)
+    // console.log(element, iteration)
+})
