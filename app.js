c334ac916e0eecce243fedc6170c95890444eba9
aatifkatote <aatifkatote@gmail.com>
2021-09-12 15:28:03 +0530

    ES6 Methods

--- app.js
+++ app.js
@@ -1,21 +1,75 @@
-//Iteration over array
-const arr = [1, 2 , 3, 4, 5]
+console.log("MERGED CLASS 1 ES6")
 
-// Basic Method for printing
-// for(var i=0; i<arr.length; i++){
-//     console.log(arr[i])
+var arr = [1,2,3,4];
+console.log(arr[2]);
+
+var [one, two, three, four, five] = arr; //ES6 Destructuring
+console.log(three);
+console.log(four);
+console.log(five);
+
+
+function getscores() {
+    return [90,100];
+}
+
+let [x,y,z] = getscores();
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+const obj = {
+    fullname: "Aatif Katote",
+    email: "aatifkatote@gmail.com",
+    age: 18
+}
+
+console.log(obj.email)
+
+let {dum, age, email, fullname} = obj;
+console.log(fullname)
+console.log(dum)
+
+
+let person = {
+    firstname: "Aatif",
+    lastname: "Katote",
+    age1: 18,
+    middlename: "DNA"
+}
+let {age1, firstname, lastname, middlename="" } = person;
+console.log(age1);
+console.log(firstname);
+console.log(lastname);
+console.log(middlename);
+
+// function add(x, y) {
+//     return x+y;
 // }
+// console.log(add(10, 20));
+
+// let add = function(x,y) {
+//     return x+y;
+// }
+// console.log(add(10,20))
+
+let add = (x, y) => {
+  return x + y;
+};
+console.log(add(10, 20));
 
 
+const counter = {
+    count: 1,
+    aatif:function(){
+        return ++(this.count)
+    },
+    //key: this.location.href
+}
 
-// For Each
-arr.forEach((element, iteration) => {
-    console.log('iteration')
-    console.log(element)
-    // console.log(element, iteration)
-})
-
-let age = 19
-let Name = "Aatif"
-console.log('Your age is ' + age)
-console.log(`Hi ${Name} Your age is ${age}`)
+console.log(counter)
+console.log(counter.aatif())
+console.log(counter.aatif())
+console.log(counter.aatif())
+console.log(counter)
